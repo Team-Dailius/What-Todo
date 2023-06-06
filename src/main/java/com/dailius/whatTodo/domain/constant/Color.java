99e0ae49e03600ff37751d108ad24070d46ad206
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-06-06 09:37:27 +0900

    # 12 - enum 타입 구현
    `컬러 코드`와 `공개 범위` enum의 구현을 진행함.
    `컬러 코드` 같은 경우, 추후 색상 추가 논의를 진행해야 함.

--- src/main/java/com/dailius/whatTodo/domain/constant/Color.java
+++ src/main/java/com/dailius/whatTodo/domain/constant/Color.java
@@ -1,4 +1,17 @@
 package com.dailius.whatTodo.domain.constant;
 
 public enum Color {
+    RED("#B82647"), CORAL("#FF7F50"),
+    ORANGE("#FF8C00"), YELLOW("#FEE134"),
+    PINK("#F53FCC");
+
+    private final String colorHexCode;
+
+    Color(String colorHexCode) {
+        this.colorHexCode = colorHexCode;
+    }
+
+    public String getColorHexCode() {
+        return colorHexCode;
+    }
 }
